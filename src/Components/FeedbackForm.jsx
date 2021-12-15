cbf357948823c72f3217f94a28489f7c5a784d7d
D_Zucho <danny.nico.dev@outlook.com>
2021-12-14 18:22:58 -0800

    start input form - connect to state

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'
+
 import Card from './shared/Card'
 
 const FeedbackForm = () => {
+  const [text, setText] = useState('')
+
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+
   return (
     <Card>
       <form>
         <h2>How would you rate us?</h2>
         {/*@TODO - rating select component */}
+        <div className='input-group'>
+          <input
+            type='text'
+            placeholder='Write a review'
+            onChange={handleTextChange}
+            value={text}
+          />
+          <button type='submit'>Send</button>
+        </div>
       </form>
     </Card>
   )
